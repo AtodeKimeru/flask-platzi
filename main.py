46f19ce63ea894e1aa0d9601977bfcaf560bdf9b
Ivan Camilo <ibarragan@unal.edu.co>
2022-07-24 07:01:28 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+
+app = Flask(__name__)
+
+todos = ['scrum', 'terminar proyecto flask', 'añadir a repositorio barra de progreso CSS']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
+
+
+@app.route('/prueba')
+def prueba():
+    raise(Exception('500 error'))
+
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+
+    return render_template('hello.html', **context)
