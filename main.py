f6da7f9d909739681990ef1028fa062ee9ad8348
IvanC <ibarragan@unal.edu.co>
2022-09-06 00:54:20 -0500

    Extensiones de Flask

--- main.py
+++ main.py
@@ -1,41 +1,88 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import (
+    Flask,
+    request,
+    make_response,
+    redirect,
+    render_template,
+    session,
+    url_for,
+    flash,
+)
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import unittest
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-todos = ['scrum', 'terminar proyecto flask', 'añadir a repositorio barra de progreso CSS']
+app.config["SECRET_KEY"] = "SUPER SECRETO"
+
+todos = [
+    "scrum",
+    "terminar proyecto flask",
+    "añadir a repositorio barra de progreso CSS",
+]
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Send")
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html', error=error)
+    return render_template("404.html", error=error)
 
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    return render_template('500.html', error=error)
+    return render_template("500.html", error=error)
 
 
-@app.route('/prueba')
+@app.route("/prueba")
 def prueba():
-    raise(Exception('500 error'))
+    raise (Exception("500 error"))
 
 
-@app.route('/')
+@app.route("/")
 def index():
     user_ip = request.remote_addr
 
-    response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    response = make_response(redirect("/hello"))
+    session["user_ip"] = user_ip
 
     return response
 
 
-@app.route('/hello')
+@app.route("/hello", methods=["GET", "POST"])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get("user_ip")
+    login_form = LoginForm()
+    username = session.get("username")
+
     context = {
-        'user_ip': user_ip,
-        'todos': todos
+        "user_ip": user_ip,
+        "todos": todos,
+        "login_form": login_form,
+        "username": username,
     }
 
-    return render_template('hello.html', **context)
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session["username"] = username
+
+        flash('¡Nombre de usuario registrado con éxito!')
+
+        return redirect(url_for("index"))
+
+    return render_template("hello.html", **context)
