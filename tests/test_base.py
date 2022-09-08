540acb5f211386b8ee2aa8a06e978c26ae7f7aba
IvanC <ibarragan@unal.edu.co>
2022-09-08 03:52:47 -0500

    blueprint auth

--- tests/test_base.py
+++ tests/test_base.py
@@ -11,27 +11,54 @@ class MainTest(TestCase):
         
         return app
 
+    
     def test_app_exists(self):
         self.assertIsNotNone(current_app)
+    
     
     def test_app_in_test_mode(self):
         self.assertTrue(current_app.config['TESTING'])
 
+    
     def test_index_redirects(self):
         response = self.client.get(url_for('index'))
 
         self.assertEqual(response.location, url_for('hello'))
 
+    
     def test_hello_get(self):
         response = self.client.get(url_for('hello'))
 
         self.assert200(response)
 
+    
     def test_hello_post(self):
+        response = self.client.post(url_for('hello'))
+
+        self.assertTrue(response.status_code, 405)
+
+    
+    def test_auth_blueprint_exist(self):
+        self.assertIn('auth', self.app.blueprints)
+
+    
+    def test_auth_login_get(self):
+        response = self.client.get(url_for('auth.login'))
+
+        self.assert200(response)
+
+    
+    def test_auth_login_template(self):
+        self.client.get(url_for('auth.login'))
+
+        self.assertTemplateUsed('login.html')
+
+
+    def test_auth_login_post(self):
         fake_form = {
             'username': 'fake',
             'password': 'fake-password',
         }
-        response = self.client.post(url_for('hello'), data=fake_form)
 
-        self.assertEqual(response.location, url_for('index'))
+        response = self.client.post(url_for('auth.login'), data=fake_form)
+        self.assertEqual(response, url_for('index'))
